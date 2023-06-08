b33793815e14b30c382081513347b855fc53b025
zhevrakov.ra <zhevrakov.ra.ki25@mail.ru>
2023-06-08 13:10:46 +0300

    jj

--- src/task_one/DoubleArray.java
+++ src/task_one/DoubleArray.java
@@ -7,6 +7,16 @@ import java.util.Scanner;
 import static java.lang.Math.round;
 
 public class DoubleArray {
+    private double[] array;
+
+    public DoubleArray(int length) {
+        array = new double[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = Math.random() * 10;
+        }
+    }
+
     Scanner scanner = new Scanner(System.in);
 
     public void methodThatDiscardsFractionalPart() {
@@ -33,7 +43,40 @@ public class DoubleArray {
 
         }
     }
-    public void sortingOriginalArrayInAscendingOrder(){
-        
+
+    public void sortingOriginalArrayInAscendingOrder(int[] integerArray) {
+        {
+            System.out.println("Integer array (with rounding):");
+            for (int num : integerArray) {
+                System.out.print(Math.round(num) + " ");
+            }
+            System.out.println();
+        }
     }
+
+    void sortOriginalArray
+            () {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Перестановка элементов массива
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void printSumOfPositiveNumbers
+            () {
+        double sum = 0;
+        for (double num : array) {
+            if (num > 0) {
+                sum += num;
+            }
+        }
+        System.out.println("Sum of positive numbers: " + sum);
+    }
+
 }
